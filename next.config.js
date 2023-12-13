f3263dfdd9e388c0f2944c3c40e63b57b532d082
lee <eric@youland.com>
2023-12-13 08:53:05 +0800

    build: add dependencies

--- next.config.js
+++ next.config.js
@@ -1,6 +1,25 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
 
-module.exports = nextConfig
+module.exports = {
+  env: {
+    USER_POOL_ID: process.env.USER_POOL_ID,
+    CLIENT_ID: process.env.CLIENT_ID,
+    STRIPE_PUBLISH_KEY: process.env.STRIPE_PUBLISH_KEY,
+  },
+  reactStrictMode: false,
+  trailingSlash: true,
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: ['@svgr/webpack'],
+    });
+    return config;
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  images: {
+    domains: ['youland-common-images.s3.us-west-1.amazonaws.com'],
+  },
+};

0083e81480f20752efc3f78dd91bb3dab363ad2b
lee <eric@youland.com>
2023-12-20 13:52:49 +0800

    feat: finish documents

--- src/components/molecules/Loan/LoanDocumentsGrid.tsx
+++ src/components/molecules/Loan/LoanDocumentsGrid.tsx
@@ -1,42 +1,45 @@
-import { FC } from 'react';
-import { Typography } from '@mui/material';
+import React, { FC, useState } from 'react';
+import { Stack, Tab, Tabs, Typography } from '@mui/material';
+import { GetAppOutlined, VisibilityOutlined } from '@mui/icons-material';
 import { DataGrid, GridColDef } from '@mui/x-data-grid';
 
 import { observer } from 'mobx-react-lite';
 
 import { utils } from '@/utils';
 
-import {
-  PortfolioGridPagination,
-  PortfolioGridToolbar,
-} from '@/components/molecules';
+import { PortfolioGridPagination } from '@/components/molecules';
 
 const mockData = [
   {
     id: 123,
-    dateReceived: '11/1/2023',
-    dateDue: '11/1/2023',
-    pmtDay: 0,
-    reference: 2787035,
-    ach: 'ACH',
-    paymentType: 'RegPmt',
-    totalPmt: utils.formatDollar(30387.5),
-    totalInterest: utils.formatDollar(0),
-    defaultInterest: utils.formatDollar(0),
-    noteInterest: utils.formatDollar(0),
-    principal: utils.formatDollar(0),
-    lateCharges: utils.formatDollar(0),
-    reservePmt: utils.formatDollar(0),
-    reserveRestricted: utils.formatDollar(0),
-    additionalInformation:
-      'This is a test sentence, This is a test sentence, This is a test sentence.',
+    description:
+      'This is a test sentence, This is a test sentence, This is a test sentence!',
+    createdDate: '11/13/2023',
+  },
+  {
+    id: 234,
+    description:
+      'This is a test sentence, This is a test sentence, This is a test sentence!',
+    createdDate: '11/13/2023',
   },
 ];
 
 const columns: GridColDef[] = [
   {
-    headerName: 'Date received',
-    field: 'dateReceived',
+    headerName: 'Description',
+    field: 'description',
+    sortable: true,
+    flex: 6,
+    align: 'center',
+    headerAlign: 'center',
+    minWidth: 180,
+    renderCell: ({ value }) => (
+      <Typography overflow={'hidden'}>{value}</Typography>
+    ),
+  },
+  {
+    headerName: 'Created date',
+    field: 'createdDate',
     sortable: true,
     flex: 1,
     align: 'center',
@@ -46,7 +49,82 @@ const columns: GridColDef[] = [
       <Typography overflow={'hidden'}>{value}</Typography>
     ),
   },
+  {
+    headerName: 'Actions',
+    field: 'actions',
+    type: 'actions',
+    renderCell: ({ id }) => (
+      <Stack flexDirection={'row'} gap={1}>
+        <VisibilityOutlined key={`${id}_preview`} sx={{ cursor: 'pointer' }} />
+        <GetAppOutlined key={`${id}_download`} sx={{ cursor: 'pointer' }} />
+      </Stack>
+    ),
+  },
 ];
+
+const LoanDocumentsGridToolbar: FC = () => {
+  const [value, setValue] = useState('one');
+
+  const handleChange = (event: React.SyntheticEvent, newValue: string) => {
+    setValue(newValue);
+  };
+
+  return (
+    <Tabs
+      onChange={handleChange}
+      sx={{
+        px: 3,
+        pt: 3,
+        pb: 1.5,
+        '& .Mui-selected': {
+          color: '#202939 !important',
+        },
+        '& .MuiTabs-indicator': {
+          display: 'flex',
+          justifyContent: 'center',
+          bgcolor: 'transparent',
+          bottom: 4,
+        },
+        '& .MuiTabs-indicatorSpan': {
+          width: '100%',
+          backgroundColor: 'primary.main',
+        },
+      }}
+      TabIndicatorProps={{
+        children: <span className={'MuiTabs-indicatorSpan'} />,
+      }}
+      value={value}
+    >
+      <Tab
+        label={'Statements'}
+        sx={{ textTransform: 'none', p: 0, fontSize: 14, fontWeight: 600 }}
+        value="one"
+      />
+      <Tab
+        label={'Year-End(1098)'}
+        sx={{
+          textTransform: 'none',
+          p: 0,
+          ml: 2,
+          fontSize: 14,
+          fontWeight: 600,
+        }}
+        value="two"
+      />
+      <Tab
+        label={'Late Notices'}
+        sx={{
+          textTransform: 'none',
+          p: 0,
+          ml: 2,
+          fontSize: 14,
+          fontWeight: 600,
+        }}
+        value="three"
+      />
+    </Tabs>
+  );
+};
 
 export const LoanDocumentsGrid: FC = observer(() => {
   const lastChildIndex = columns.length;
@@ -59,7 +137,7 @@ export const LoanDocumentsGrid: FC = observer(() => {
       rowHeight={64}
       rows={mockData}
       slots={{
-        toolbar: PortfolioGridToolbar,
+        toolbar: LoanDocumentsGridToolbar,
         //footer: PortfolioGridPagination,
         pagination: PortfolioGridPagination,
       }}

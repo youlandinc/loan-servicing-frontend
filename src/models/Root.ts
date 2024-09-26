7949721be7260554a5e1d09c7a0dc393beeaf5ca
alfred <alfred@youland.com>
2024-09-26 17:03:18 +0800

    refactor: missing

--- src/models/Root.ts
+++ src/models/Root.ts
@@ -6,6 +6,7 @@ import {
   DelinquentTimeRangeEnum,
   MaturityTimeRangeEnum,
   PipelineMode,
+  PortfolioGridTypeEnum,
 } from '@/types/enum';
 
 import { URL_LOGOUT_REDIRECTION } from '@/constant';
